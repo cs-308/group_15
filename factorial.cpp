637f1869d66a831d3c06b9046198bbf7c4aed699
Titiksha-02 <b20138@students.iitmandi.ac.in>
2022-09-05 15:43:48 +0530

    negative numbers

--- factorial.cpp
+++ factorial.cpp
@@ -2,10 +2,7 @@
 
 int factorial(int n){
     if(n<0){
-    	printf("Enter a positive number :");
-    	int a;
-    	cin>>a;
-    	return factorial(a);
+    	return -1;
     }
     else if(n!=1){
 	return(n * factorial(n-1));
