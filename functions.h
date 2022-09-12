9aad281ddad03d87e41f30635796a3daa0aebed3
Nilesh Rewasiya <b20117@students.iitmandi.ac.in>
2022-09-12 12:38:47 +0530

    Recursive output in main.cpp

--- functions.h
+++ functions.h
@@ -12,3 +12,15 @@ int gcd(int a, int b){
     }
     return a;
 }
+
+int gcdRecu(int a, int b){
+    if(b==0){
+        return a;
+    }
+    if(a>b){
+        return gcdRecu(b, a%b);
+    }
+    else{
+        return gcdRecu(a, b%a);
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include"functions.h"
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 56;
+    int y = 98;
+    // cout<<gcd(x,y)<<endl;
+    cout<<gcdRecu(x,y)<<endl;
+    return 0;
+}
